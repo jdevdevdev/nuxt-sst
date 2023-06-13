3e68f7646de682fb8713b13480cac1a38007793e
jdevdevdev <117501493+jdevdevdev@users.noreply.github.com>
2023-06-13 15:40:09 +1000

    inital sst creation

---
+++ sst.config.ts
@@ -0,0 +1,22 @@
+import { SSTConfig } from "sst";
+import { NuxtSite } from "./stacks/NuxtSite";
+
+export default {
+  config(_input) {
+    return {
+      name: "nuxtsst",
+      region: "ap-southeast-2",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NuxtSite(stack, "site", {
+        path: "nuxt-app/"
+      });
+
+      stack.addOutputs({
+        url: site.url,
+      });
+    });
+  }
+} satisfies SSTConfig;
